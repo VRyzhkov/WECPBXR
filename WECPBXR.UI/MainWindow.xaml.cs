7bcbe5431e61ef2855a47dbd3a05e78db883deec
ryzhkov <ryzhkov@asc-ural.ru>
2026-08-27 10:20:08 +0500

    Added pin and resize form

--- WECPBXR.UI/MainWindow.xaml.cs
+++ WECPBXR.UI/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Input.Platform;
 using Avalonia.Interactivity;
+using Avalonia.Media;
 using Avalonia.VisualTree;
 using WECPBXR.UI.ViewModels;
 
@@ -11,7 +12,14 @@ namespace WECPBXR.UI;
 
 public partial class MainWindow : Window
 {
+    private const double StandardWindowWidth = 1000;
+    private const double StandardWindowHeight = 760;
+    private const double CompactWindowScale = 0.5;
+    private const string ReduceWindowIconData = "M 2 2 L 6 6 M 6 6 L 6 3 M 6 6 L 3 6 M 12 12 L 8 8 M 8 8 L 11 8 M 8 8 L 8 11";
+    private const string RestoreWindowIconData = "M 6 6 L 2 2 M 2 2 L 5 2 M 2 2 L 2 5 M 8 8 L 12 12 M 12 12 L 9 12 M 12 12 L 12 9";
+
     private readonly MainWindowViewModel _viewModel;
+    private bool _isCompact;
 
     public MainWindow()
     {
@@ -31,6 +39,36 @@ public partial class MainWindow : Window
         {
             BeginMoveDrag(e);
         }
+    }
+
+    private void ScaleButton_Click(object? sender, RoutedEventArgs e)
+    {
+        _isCompact = !_isCompact;
+
+        double previousWidth = Width;
+        double previousHeight = Height;
+        double nextWidth = _isCompact ? StandardWindowWidth * CompactWindowScale : StandardWindowWidth;
+        double nextHeight = _isCompact ? StandardWindowHeight * CompactWindowScale : StandardWindowHeight;
+
+        MinWidth = nextWidth;
+        MinHeight = nextHeight;
+        Width = nextWidth;
+        Height = nextHeight;
+
+        Position = new PixelPoint(
+            Position.X + (int)Math.Round((previousWidth - nextWidth) / 2),
+            Position.Y + (int)Math.Round((previousHeight - nextHeight) / 2));
+
+        ScaleIcon.Data = Geometry.Parse(_isCompact ? RestoreWindowIconData : ReduceWindowIconData);
+        ToolTip.SetTip(ScaleButton, _isCompact ? "Restore window size" : "Reduce window size");
+    }
+
+    private void TopmostButton_Click(object? sender, RoutedEventArgs e)
+    {
+        Topmost = !Topmost;
+
+        TopmostPinIcon.RenderTransform = Topmost ? new RotateTransform(45) : null;
+        ToolTip.SetTip(TopmostButton, Topmost ? "Unpin window" : "Pin window on top");
     }
 
     private void MinimizeButton_Click(object? sender, RoutedEventArgs e)
